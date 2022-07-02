00976fbf871a394fbeabc4e701377d8cd56da66d
NikiBober <95879723+NikiBober@users.noreply.github.com>
2022-07-02 18:02:16 +0300

    v 0.7 Add player movement
    by keys. For correct detecting is player in range of seller i set another layer to player and edit collision matrix to not detect collision on the same layer.

--- LSW Programming Interview/Assets/Scripts/PlayerController.cs
+++ LSW Programming Interview/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+    Rigidbody2D rigidbody2d;
+    float horizontalInput;
+    float verticalInput;
+    Vector2 position;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        position = rigidbody2d.position;
+        position.x += horizontalInput * speed;
+        position.y += verticalInput * speed;
+
+        rigidbody2d.MovePosition(position);
     }
 }
